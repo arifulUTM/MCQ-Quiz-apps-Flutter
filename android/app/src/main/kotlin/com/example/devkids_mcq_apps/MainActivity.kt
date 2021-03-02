e4ce3a4805644825c45ce7a956b88d237e0e8be3
Md Ariful Islam <jahorularif@gmail.com>
2021-03-02 20:50:28 +0800

    JSON to dart added in quiz.dart

---
+++ android/app/src/main/kotlin/com/example/devkids_mcq_apps/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.devkids_mcq_apps
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: HomePage(),
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primaryColor: Colors.white));
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Multiple Choice Question"),
+        elevation: 0.0,
+      ),
+      body: Container(),
+    );
+  }
+}
